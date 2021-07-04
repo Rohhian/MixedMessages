59145307dbfc80d14ca2ec7a72537731efc617c6
Rohhian <kristianmiilen@gmail.com>
2021-07-04 16:48:03 +0300

    initialized and populated 3 arrays with words

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,1 +1,4 @@
 // this is my first Portfolio Project called Mixed Messages
+const nouns = ['car', 'plane', 'cat', 'dog', 'house', 'bike', 'road', 'man', 'woman', 'child', 'friend', 'enemy'];
+const verbs = ['driving', 'talking to', 'singing to', 'running from', 'loving', 'jumping on', 'avoiding', 'walking towards', 'running away'];
+const adjectives = ['red', 'green', 'violet', 'blue', 'yellow', 'angry', 'friendly', 'heavy', 'lightweight', 'fat', 'skinny', 'shitty', 'amazing', 'sleepy', 'sexy', 'drunk'];
