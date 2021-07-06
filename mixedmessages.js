c157d8ab5bc9802ad94a9de40125fc68c28378b4
Rohhian <kristianmiilen@gmail.com>
2021-07-06 12:27:22 +0300

    changed on item in the verbs array

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,6 +1,6 @@
 // this is my first Portfolio Project called Mixed Messages
 const nouns = ['car', 'plane', 'cat', 'dog', 'house', 'bike', 'road', 'man', 'woman', 'child', 'friend', 'enemy'];
-const verbs = ['driving', 'talking to', 'singing to', 'running from', 'loving', 'jumping on', 'avoiding', 'walking towards', 'running away'];
+const verbs = ['driving', 'talking to', 'singing to', 'running away from', 'loving', 'jumping on', 'avoiding', 'walking towards'];
 const adjectives = ['red', 'green', 'violet', 'blue', 'yellow', 'angry', 'friendly', 'heavy', 'lightweight', 'fat', 'skinny', 'shitty', 'amazing', 'sleepy', 'sexy', 'drunk'];
 
 let n = Math.floor(Math.random() * nouns.length);
