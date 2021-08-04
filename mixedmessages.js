76cf46034122340416f381cca7bbb97ef8b3c5bf
Rohhian <kristianmiilen@gmail.com>
2021-08-04 15:58:59 +0300

    bugfixes to previous

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,7 +1,7 @@
 // this is my first Portfolio Project called Mixed Messages
 const nouns = ['car', 'plane', 'cat', 'dog', 'house', 'bike', 'road', 'man', 'woman', 'child', 'friend', 'enemy'];
 const verbs = ['driving', 'talking to', 'singing to', 'running away from', 'loving', 'jumping on', 'avoiding', 'walking towards'];
-const adjectives = ['red', 'green', 'violet', 'blue', 'yellow', 'angry', 'friendly', 'heavy', 'lightweight', 'fat', 'skinny', 'shitty', 'amazing', 'sleepy', 'sexy', 'drunk', ];
+const adjectives = ['red', 'green', 'violet', 'blue', 'yellow', 'angry', 'friendly', 'heavy', 'lightweight', 'fat', 'skinny', 'shitty', 'amazing', 'sleepy', 'sexy', 'drunk'];
 
 let n = Math.floor(Math.random() * nouns.length);
 let v = Math.floor(Math.random() * verbs.length);
@@ -9,7 +9,7 @@ let a = Math.floor(Math.random() * adjectives.length);
 let n1 = Math.floor(Math.random() * nouns.length);
 while (n1 === n) {
     n1 = Math.floor(Math.random() * nouns.length);
-}
+};
 
 // console.log(`Your ${adjectives[a]} ${nouns[n]} is ${verbs[v]} the ${nouns[n1]}`);
 
