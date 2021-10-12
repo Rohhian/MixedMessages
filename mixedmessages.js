e746c6820864759ca094fac9652ca86dc7ad2809
Kristian Miilen <86469714+Rohhian@users.noreply.github.com>
2021-10-12 13:24:14 +0300

    Update mixedmessages.js
    puhastasin mõned välja-kommenteeritud read ära

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,4 +1,3 @@
-// this is my first Portfolio Project called Mixed Messages
 const nouns = ['car', 'plane', 'cat', 'dog', 'house', 'bike', 'road', 'man', 'woman', 'child', 'friend', 'enemy', 'student', 'teacher', 'politician', 'gangsta', 'doctor', 'chemist'];
 const verbs = ['driving', 'talking to', 'singing to', 'running away from', 'loving', 'jumping on', 'avoiding', 'walking towards', 'screaming at', 'digging a hole to', 'apologizing to', ];
 const adjectives = ['red', 'green', 'violet', 'blue', 'yellow', 'angry', 'friendly', 'heavy', 'lightweight', 'fat', 'skinny', 'shitty', 'amazing', 'sleepy', 'sexy', 'drunk', 'sad', 'happy', ];
@@ -12,8 +11,6 @@ while (n1 === n) {
     n1 = Math.floor(Math.random() * nouns.length);
 };
 let w = Math.floor(Math.random() * who.length);
-
-// console.log(`Your ${adjectives[a]} ${nouns[n]} is ${verbs[v]} the ${nouns[n1]}`);
 
 let messageBox = document.getElementById('message');
 let message = `${who[w]} ${adjectives[a]} ${nouns[n]} is ${verbs[v]} the ${nouns[n1]}`;
